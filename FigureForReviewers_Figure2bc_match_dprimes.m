2899a34d7147c9a774e92bec5dee0cb08a028971
Marino Pagan <marinopagan@gmail.com>
2020-10-12 04:24:04 -0400

    added classifiers comparison SC and PFC with matched d primes

---
+++ FigureForReviewers_Figure2bc_match_dprimes.m
@@ -0,0 +1,176 @@
+clear
+clc
+close all
+
+% load data_files/svm_results_sc
+% load data_files/svm_results_pfc
+% load data_files/svm_results_fof
+
+load data_files/svm_results_sc_pfc_matched_dprimes
+% res_choice_sc res_choice_pfc
+
+
+save_figures=0;
+
+
+%%% x axis limit (time)
+xlimit=[-1.9 0.7];
+
+%%% y axis limit (performance)
+ylimit=[0.35 1.02];
+
+
+
+
+
+%%%%%%%%% COMPARE SC AND PFC - CHOICE
+figure
+plot_classifier_results(centers,res_choice_sc,'b',xlimit,ylimit)
+hold on
+plot_classifier_results(centers,res_choice_pfc,'b--',xlimit,ylimit)
+title('Task: choice')
+legend('SC','PFC','Location','Best')
+legend BOXOFF
+% ax = gca;
+% ax.Position = [0.1300 0.1437 0.7750 0.45];
+% if(save_figures)
+%     saveas(gcf,'figures/classifier_correct_choice','pdf')
+% end
+% 
+
+
+vals=0.7;
+
+balo1=nan(size(res_choice_sc,1),length(vals));
+for i=1:size(res_choice_sc,1)
+    vec=res_choice_sc(i,:);
+    vec=smooth(vec)';
+    for j=1:length(vals)
+%         ind=find(centers>=-.125 & vec>=vals(j));
+        ind=find(centers>=-1 & vec>=vals(j));
+        balo1(i,j)=centers(ind(1));
+    end
+end
+
+balo2=nan(size(res_choice_pfc,1),length(vals));
+for i=1:size(res_choice_pfc,1)
+    vec=res_choice_pfc(i,:);
+    vec=smooth(vec)';
+    for j=1:length(vals)
+%         ind=find(centers>=-.125 & vec>=vals(j));
+        ind=find(centers>=-1 & vec>=vals(j));
+        balo2(i,j)=centers(ind(1));
+    end
+end
+
+
+
+%%%% shift the time axis to make plot "causal" (e.g. no choice information before 0)
+centers_back=0.125;
+balo1=balo1+centers_back;
+balo2=balo2+centers_back;
+% balo3=balo3+centers_back;
+
+mean(balo2-balo1)
+std(balo2-balo1)
+
+% 
+% mean(balo3-balo1)
+% std(balo3-balo1)
+
+
+mean(balo1)
+std(balo1)
+
+
+
+
+
+x=vals;
+y1=mean(balo1);
+y2=std(balo1);
+colo='b';
+
+%     %% plot!
+vec1=y1+y2;
+vec2=y1-y2;
+hold on
+plot([vec1 vec2],[x x],colo,'LineWidth',5)
+hold on
+
+
+
+x=vals;
+y1=mean(balo2);
+y2=std(balo2);
+colo='b';
+
+%     %% plot!
+vec1=y1+y2;
+vec2=y1-y2;
+hold on
+plot([vec1 vec2],[x x],colo,'LineWidth',5)
+hold on
+
+
+% 
+% x=vals;
+% y1=mean(balo3);
+% y2=std(balo3);
+% colo='r';
+% 
+% %     %% plot!
+% vec1=y1+y2;
+% vec2=y1-y2;
+% hold on
+% plot([vec1 vec2],[x x],colo,'LineWidth',5)
+% hold on
+
+
+
+return
+
+x=vals;
+y1=mean(balo1);
+y2=std(balo1);
+colo='b';
+
+%     %% plot!
+vec1=y1+y2;
+vec2=y1-y2;
+hold on
+plot(y1,x,colo,'LineWidth',2)
+hold on
+plot([vec1 vec2(end:-1:1) vec1(1)],[x x(end:-1:1) x(1)],colo,'LineWidth',1)
+hold on
+
+
+
+
+x=vals;
+y1=mean(balo2);
+y2=std(balo2);
+colo='b';
+
+%     %% plot!
+vec1=y1+y2;
+vec2=y1-y2;
+hold on
+plot(y1,x,colo,'LineWidth',2)
+hold on
+plot([vec1 vec2(end:-1:1) vec1(1)],[x x(end:-1:1) x(1)],colo,'LineWidth',1)
+hold on
+
+x=vals;
+y1=mean(balo3);
+y2=std(balo3);
+colo='b';
+
+%     %% plot!
+vec1=y1+y2;
+vec2=y1-y2;
+hold on
+plot(y1,x,colo,'LineWidth',2)
+hold on
+plot([vec1 vec2(end:-1:1) vec1(1)],[x x(end:-1:1) x(1)],colo,'LineWidth',1)
+hold on
--- classifier_code/compute_classifiers_sc_pfc_match_dprimes.m
+++ classifier_code/compute_classifiers_sc_pfc_match_dprimes.m
@@ -20,13 +20,25 @@ max(mean(dp_choice_pfc.^2))
 
 
 
-num=134;
+num=183;
 
-%index 72 = timepoint 0
-vals=mean(dp_choice_pfc(:,72:end).^2,2);
-[~,ind]=sort(vals,'descend');
-dp_choice_pfc=dp_choice_pfc(ind(1:num),:);
+% % % %index 72 = timepoint 0
+% % % vals=mean(dp_choice_pfc(:,72:end).^2,2);
 
+
+vals=mean(dp_choice_sc.^2,2);
+[~,ind_sc]=sort(vals,'ascend');
+dp_choice_sc=dp_choice_sc(ind_sc(1:num),:);
+
+vals=mean(dp_choice_pfc.^2,2);
+[~,ind_pfc]=sort(vals,'descend');
+dp_choice_pfc=dp_choice_pfc(ind_pfc(1:num),:);
+
+
+disp(' ')
+
+disp('max d prime SC after subselection')
+max(mean(dp_choice_sc.^2))
 
 disp(' ')
 
@@ -34,7 +46,10 @@ disp('max d prime PFC after subselection')
 max(mean(dp_choice_pfc.^2))
 
 
-matpfc=matpfc(ind(1:num),:,:,:,:);
+
+
+matsc=matsc(ind_sc(1:num),:,:,:,:);
+matpfc=matpfc(ind_pfc(1:num),:,:,:,:);
 
 cd classifier_code
 
@@ -58,7 +73,87 @@ ntest=20;
 ntest_err=9;
 
 
-n_neur=29;
+n_neur=num;
+
+
+
+
+
+pop=matsc;
+
+res_choice_sc=nan(n_iterations,length(centers));
+
+
+for i=1:n_iterations
+    
+    [i n_iterations]
+    
+    for tx=1:length(centers) %look over different time points
+        
+        
+        ra_neur=randperm(size(pop,1));
+        
+
+            
+
+            %%%%% CONTRA/IPSI
+            train_contra=nan(n_neur,ntrain);
+            train_ipsi=nan(n_neur,ntrain);
+            
+            test_contra=nan(n_neur,ntest);
+            test_ipsi=nan(n_neur,ntest);
+             
+            
+            
+            for nx=1:n_neur
+                
+
+                %%%%% CHOICE
+                
+                %%%%% CORRECT CONTRA
+                mat=squeeze(pop(:,tx,:,:,1));
+                mat=mat(:,:);
+                ra=randperm(size(mat,2));
+                train_contra(nx,:)=mat(ra_neur(nx),ra(1:ntrain));
+                test_contra(nx,:)=mat(ra_neur(nx),ra(ntrain+1:ntrain+ntest));
+                
+                %%%%% CORRECT IPSI
+                mat=squeeze(pop(:,tx,:,:,2));
+                mat=mat(:,:);
+                ra=randperm(size(mat,2));
+                train_ipsi(nx,:)=mat(ra_neur(nx),ra(1:ntrain));
+                test_ipsi(nx,:)=mat(ra_neur(nx),ra(ntrain+1:ntrain+ntest));
+                
+            end
+            
+        
+        
+
+        %%%%%% classify CHOICE
+        
+        %%% run svm classifier
+        [w,k]=svm_train(train_contra,train_ipsi,cost);
+        
+        %%% classify points (CORRECT)
+        valp=w'*test_contra + k>0; % classified as Pro
+        vala=w'*test_ipsi + k<=0; % classified as Anti
+        res_choice_sc(i,tx)=mean([valp vala]);
+        
+        
+    end
+    
+end
+
+
+
+
+
+
+
+
+
+
+
 
 pop=matpfc;
 
@@ -71,7 +166,6 @@ for i=1:n_iterations
     
     for tx=1:length(centers) %look over different time points
         
-        %%%%% pfc
         
         ra_neur=randperm(size(pop,1));
         
@@ -127,7 +221,8 @@ for i=1:n_iterations
 end
 
 
-save ../data_files/svm_results_pfc_most_informative_29 res_choice_pfc  centers
+save ../data_files/svm_results_sc_pfc_matched_dprimes ...
+    res_choice_sc res_choice_pfc centers
 
 
 
